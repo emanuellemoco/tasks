e879bfe6c1d2b251c71190727c4dceade2e35ecb
Emanuelle <emanuellesmoco@gmail.com>
2020-11-20 10:48:51 -0300

    updating script

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+#How to use serializer from https://www.askpython.com/django/django-listview
 
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from .models import Task
+from .serializer import Serializer
+from django.http import JsonResponse 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+def get_task(request: HttpRequest):
+    all_tasks = Task.objects.all()#.order_by('-date_joined')
+    serializer = Serializer(all_tasks, many =True)
+    return JsonResponse(serializer.data, safe =False)
+
+
+    
